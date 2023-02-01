8957be3ff36a7606b2fcb714b9e634a8632661ca
drewherald <drewherald9@gmail.com>
2023-02-01 10:10:53 -0500

    everything done but game function

--- js.js
+++ js.js
@@ -3,25 +3,31 @@ const gameBoard = (() => {
     let row2 = [0,0,0];
     let row3 = [0,0,0];
 
+
+    return {row1, row2, row3};
+});
+
+const gameInterface = (board) => {
+
     let win = () => {
-        if(row1[0]===1 && row2[0]===1 && row3[0]===1 || row1[1]===1 && row2[1]===1 && row3[1]===1 || row1[2]===1 && row2[2]===1 && row3[2]===1){
+        if(board.row1[0]===1 && board.row2[0]===1 && board.row3[0]===1 || board.row1[1]===1 && board.row2[1]===1 && board.row3[1]===1 || board.row1[2]===1 && board.row2[2]===1 && board.row3[2]===1){
             return true;
         };
-        if(row1[0]===2 && row2[0]===2 && row3[0]===2 || row1[1]===2 && row2[1]===2 && row3[1]===2 || row1[2]===2 && row2[2]===2 && row3[2]===2){
-            return true;
-        };
-    
-        if(row1[0]===1 && row1[1]===1 && row1[2]===1 || row2[0]===1 && row2[1]===1 && row2[2]===1 || row3[0]===1 && row3[1]===1 && row3[2]===1){
-            return true;
-        };
-        if(row1[0]===2 && row1[1]===2 && row1[2]===2 || row2[0]===2 && row2[1]===2 && row2[2]===2 || row3[0]===2 && row3[1]===2 && row3[2]===2){
-            return true;
-        };
-        if(row1[0]===1 && row2[1]===1 && row3[2]===1 || row1[2]===1 && row2[1]===1 && row3[0]===1){
+        if(board.row1[0]===2 && board.row2[0]===2 && board.row3[0]===2 || board.row1[1]===2 && board.row2[1]===2 && board.row3[1]===2 || board.row1[2]===2 && board.row2[2]===2 && board.row3[2]===2){
             return true;
         };
     
-        if(row1[0]===2 && row2[1]===2 && row3[2]===2 || row1[2]===2 && row2[1]===2 && row3[0]===2){
+        if(board.row1[0]===1 && board.row1[1]===1 && board.row1[2]===1 || board.row2[0]===1 && board.row2[1]===1 && board.row2[2]===1 || board.row3[0]===1 && board.row3[1]===1 && board.row3[2]===1){
+            return true;
+        };
+        if(board.row1[0]===2 && board.row1[1]===2 && board.row1[2]===2 || board.row2[0]===2 && board.row2[1]===2 && board.row2[2]===2 || board.row3[0]===2 && board.row3[1]===2 && board.row3[2]===2){
+            return true;
+        };
+        if(board.row1[0]===1 && board.row2[1]===1 && board.row3[2]===1 ||board.row1[2]===1 && board.row2[1]===1 && board.row3[0]===1){
+            return true;
+        };
+    
+        if(board.row1[0]===2 && board.row2[1]===2 && board.row3[2]===2 || board.row1[2]===2 && board.row2[1]===2 && board.row3[0]===2){
             return true;
         };
 
@@ -29,52 +35,84 @@ const gameBoard = (() => {
     };
    
 
+    const isEmpty = (item) => {
+        if(item===0){
+            return true;
+        }else{
+            return false;
+        }
+    };
+
 
     const moveX = (row, col) => {
         if(win()){
             return;
         } else{
             if(row === 1){
-                if(row1[col]!=0){
-                    row1[col] = 1;
+                if(isEmpty(board.row1[col])){
+                    board.row1[col] = 1;
                 }
             }else if(row===2){
-                if(row2[col]!=0){
-                    row2[col] = 1;
+                if(isEmpty(board.row2[col])){
+                    board.row2[col] = 1;
                 }
             }else if(row===3){
-                if(row3[col]!=0){
-                    row3[col] = 1;
+                if(isEmpty(board.row3[col])){
+                    board.row3[col] = 1;
                 }
             }
         }
        
-    }
+    };
 
     const moveO = (row, col) => {
         if(win()){
             return;
         }else{
             if(row === 1){
-                if(row1[col]!=0){
-                    row1[col] = 2;
+                if(isEmpty(board.row1[col])){
+                    board.row1[col] = 2;
                 }
             }else if(row===2){
-                if(row2[col]!=0){
-                    row2[col] = 2;
+                if(isEmpty(board.row2[col])){
+                    board.row2[col] = 2;
                 }
             }else if(row===3){
-                if(row3[col]!=0){
-                    row3[col] = 2;
+                if(isEmpty(board.row3[col])){
+                    board.row3[col] = 2;
                 }
             }
         }
-    }
+    };
 
-    return {moveX,moveO};
-});
-
-const gameInterface = (board) => {
-    
+    return {moveO, moveX, win};
 }; 
 
+const Player = () => {
+    let turn = false;
+
+    const turnStart = () => {
+        turn = true;
+    };
+
+    const turnEnd = () => {
+        turn = false;
+    };
+
+    return {turnStart, turnEnd};
+};
+
+const playBoard = new gameBoard();
+const userInterface = new gameInterface(playBoard);
+const player1 = Player();
+const player2 = Player();
+
+const first = document.querySelector("#1,0");
+const second = document.querySelector("#1,1");
+const third = document.querySelector("#1,2");
+const fourth = document.querySelector("#2,0");
+const fifth = document.querySelector("#2,1");
+const sixth = document.querySelector("#2,2");
+const seventh = document.querySelector("#3,0");
+const eighth = document.querySelector("#3,1");
+const ninth = document.querySelector("#3,2");
